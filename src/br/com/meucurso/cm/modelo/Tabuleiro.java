3c788b030e2c1c7c07266e7aa71fd6468cfa08e3
Thiago Colares <thiagocolaress77@gmail.com>
2023-07-18 22:31:44 -0300

    update tabuleiro

--- src/br/com/meucurso/cm/modelo/Tabuleiro.java
+++ src/br/com/meucurso/cm/modelo/Tabuleiro.java
@@ -1,5 +1,52 @@
 package br.com.meucurso.cm.modelo;
 
-public class Tabuleiro {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();	
+	}
+
+
+	private void sortearMinas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void associarVizinhos() {
+		for(int linha = 0; linha < linhas; linha++) {
+			for(int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
+		
+	}
+
+
+	private void gerarCampos() {
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.adicionarVizinhos(c2);
+			} 
+		}
+		
+	}
+	
+	
 }
